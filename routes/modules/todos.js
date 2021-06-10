5878591bfd2cef25dfd54892bdc1d93f3b063b4d
Ashley <ahappyday4all@gmail.com>
2021-06-10 15:56:51 +0800

    feat: add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,6 +3,19 @@ const router = express.Router()
 
 const { Todo } = require('../../models')
 
+// Create: 新增 Todo 的頁面
+router.get('/create', (req, res) => {
+	res.render('create')
+})
+
+router.post('/', (req, res) => {
+	const UserId = req.user.id
+	const { name } = req.body
+	return Todo.create({ name, UserId }) // 要把使用者 id 一起存入
+		.then(() => res.redirect('/'))
+		.catch(error => console.log(error))
+})
+
 // Read: detail page
 router.get('/:id', (req, res) => {
 	const { id } = req.params
